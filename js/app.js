28626976cf47d507f669f4c8e0d05cf4314a8553
Oscarintween <oscarintween@gmail.com>
2022-06-04 14:39:56 -0500

    agregamos header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const {getData} = require('./getData')
 const form1 = document.querySelector('#form1')
 const totalCal = document.querySelector('#totalCal')
 const container = document.querySelector('#container')
@@ -15,29 +16,7 @@ let myChart;
 
 
 //=============== FUNCTION TO BRING GROUP OF ITEMS FROM API ===============
-getData = async()=>{
-  const url = `https://trackapi.nutritionix.com/v2/search/instant?query=${form1.value}&detailed=true`
-  try {
-      let response = await fetch(url,{
-          method:'GET',
-          headers:{
-              'content-type':'application/json',
-              'x-app-id':'52961f76',
-              'x-app-key':'767fff5579e1dfc9bd9b25e883d8a276'
-          }
-      })
-      let data = await response.json()
-      foodData = data.common
-      console.log(foodData)
-      render(foodData)
-  } catch (error) {
-    container.innerHTML=`
-         Sorry nothing was found please try another food item`
-      console.log('no item found')
-  }
-  
- 
-}
+getData()
   
 
 //=============== FUNCTION TO RENDER CARD WITH FOOD ITEM INFORMATION ===============
@@ -174,7 +153,7 @@ const setChart = (info) => {
       const dataConfig = {
         labels:item.keys,
         datasets: [{
-          label: item.name,
+          label: item.name ,
           backgroundColor: 'rgb(255, 99, 132)',
           borderColor: 'rgb(255, 99, 132)',
           data:item.values,
