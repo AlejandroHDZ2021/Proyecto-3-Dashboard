c7a70f777cd7cbcbfd640196047a8a3b326facbf
Oscarintween <oscarintween@gmail.com>
2022-06-04 14:42:57 -0500

    agregar header

--- js/app.js
+++ js/app.js
@@ -16,7 +16,29 @@ let myChart;
 
 
 //=============== FUNCTION TO BRING GROUP OF ITEMS FROM API ===============
-getData()
+getData = async()=>{
+  const url = `https://trackapi.nutritionix.com/v2/search/instant?query=${form1.value}&detailed=true`
+  try {
+      let response = await fetch(url,{
+          method:'GET',
+          headers:{
+              'content-type':'application/json',
+              'x-app-id':'52961f76',
+              'x-app-key':'767fff5579e1dfc9bd9b25e883d8a276'
+          }
+      })
+      let data = await response.json()
+      foodData = data.common
+      console.log(foodData)
+      render(foodData)
+  } catch (error) {
+    container.innerHTML=`
+         Sorry nothing was found please try another food item`
+      console.log('no item found')
+  }
+  
+ 
+}
   
 
 //=============== FUNCTION TO RENDER CARD WITH FOOD ITEM INFORMATION ===============
